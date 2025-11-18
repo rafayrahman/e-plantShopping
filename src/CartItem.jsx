416e4e7cb35e6093daf18a160bdfb4539b247b23
rafayrahman <r.rafayrahman@gmail.com>
2025-11-18 06:08:26 -0500

    Integrated Redux in ProductList and CartItem; implemented total calculation, increment/decrement, remove item, and continue shopping functionality

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -12,8 +12,7 @@ const CartItem = ({ onContinueShopping }) => {
     return cart.reduce((total, item) => total + item.quantity * parseFloat(item.cost.slice(1)), 0);
   };
 
-  const handleContinueShopping = (e) => {
-    e.preventDefault();
+  const handleContinueShopping = () => {
     onContinueShopping();
   };
 
@@ -65,9 +64,9 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button" onClick={handleContinueShopping}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
